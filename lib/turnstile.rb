e7e11876e062b567276de74b6f12a60bcabf27e8
Atasay Gokkaya <atasay@wanelo.com>
2014-03-11 11:35:08 -0700

    Add configuration helpers and start with defaults

--- lib/turnstile.rb
+++ lib/turnstile.rb
@@ -1,7 +1,12 @@
-require 'redis'
 require 'turnstile/version'
 require 'turnstile/configuration'
 
 module Turnstile
+  def self.configure(&block)
+    @configuration = Turnstile::Configuration.new.configure(&block)
+  end
 
+  def self.config
+    @configuration ||= Turnstile::Configuration.new
+  end
 end
