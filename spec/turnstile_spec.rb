cf4301c6a4bf71b9d76f0343a24052dcd15f4a46
Atasay Gokkaya <atasay@wanelo.com>
2014-03-11 12:51:03 -0700

    Extra tests around tracking and observing behavior

---
+++ spec/turnstile_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require 'timecop'
+
+describe Turnstile do
+
+  let(:tracker) { Turnstile::Tracker.new }
+  let(:observer) { Turnstile::Observer.new }
+
+  let(:uid_1) { 1238438 }
+  let(:uid_2) { 1238439 }
+  let(:uid_3) { 'some_id' }
+  let(:uid_4) { 1238441 }
+  let(:uid_5) { 1238442 }
+
+  let(:previous_window) { 1394508408 }
+  let(:window) { 1394508468 }
+
+  let(:platform) { :ios }
+
+  describe "general tests" do
+    let(:expected_stats) do
+      {
+          'ios' => 2,
+          'unknown' => 1
+      }
+    end
+
+    it "tracks concurrent users online in different time windows" do
+      Timecop.freeze Time.at(previous_window) do
+        tracker.track(uid_1, platform)
+        tracker.track(uid_2, platform)
+        tracker.track(uid_3)
+      end
+      Timecop.freeze Time.at(window) do
+        tracker.track(uid_4, platform)
+        tracker.track(uid_5)
+        expect(observer.stats).to eql expected_stats
+      end
+    end
+  end
+end
